385c545c284dfe361c48dc96cf8879af45992261
Dimpal <dimpal.agrawal_cs19@gla.ac.in>
2020-08-24 19:23:05 +0530

    added a methods like default constructor, get and set method , toString(), equals() for a book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,10 +6,48 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private String bookAuthor;
     private String bookIsbnNumber;
 
+    public Book() {
+        bookName = "Java Programming";
+        bookAuthor = "Mr.Divyansh Bhardwaj";
+        bookIsbnNumber = String.valueOf(2342567892567L);
+    }
 
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookIsbnNumber='" + bookIsbnNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(bookAuthor, book.bookAuthor) &&
+                Objects.equals(bookIsbnNumber, book.bookIsbnNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthor, bookIsbnNumber);
+    }
 }
